c9dcb0b938cf7871af65a96fd3954a970bb150e6
touruni <touruni@solarOne.(none)>
2011-10-04 21:19:09 -0700

    Chapter 3

---
+++ chapter3/Birthday.java
@@ -0,0 +1,6 @@
+/* Exercise for Chapter 3 : 
+ * Create a program that turns a birthday in MM/DD/YYYY firnat 
+ * (such as 12/04/2007) into three individual strings.
+ */
+
+
---
+++ chapter3/FamilyMember.java
@@ -0,0 +1,13 @@
+
+class FamilyMember {
+    public static void main(String[] args){
+        static String surname = "Mendoza";
+        String name;
+        int age;
+        FamilyMember dad = new FamilyMember();
+        System.out.println("Family's surname is: " + dad.surname);
+        System.out.println("Family's surname is : " + FamilyMember.surname);
+}
+
+}
+
