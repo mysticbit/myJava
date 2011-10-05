61a1eceb8269f1d15c39ba9677cb915757274f1d
touruni <touruni@gmail.com>
2011-10-04 21:46:27 -0700

    finished exercise 1

--- chapter3/Birthday.java
+++ chapter3/Birthday.java
@@ -3,4 +3,19 @@
  * (such as 12/04/2007) into three individual strings.
  */
 
+import java.util.StringTokenizer;
 
+class Birthday {
+    public static void main(String args[]) {
+        String birthday = "12/04/2007";
+        String format;
+        String one, two, three;
+
+        format = birthday.replace('/', ' ');
+        
+        StringTokenizer st = new StringTokenizer(format);
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+}
+}
+}
