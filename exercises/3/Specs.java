c6c93109545b37968a56dff27c71bc94259e64a3
touruni <touruni@gmail.com>
2011-10-13 22:17:37 -0700

    changes again

---
+++ exercises/3/Specs.java
@@ -0,0 +1,14 @@
+/* Create a class with instance variables for height, 
+ * weight, and depth, making each an integer. Create a 
+ * Java application that uses your new class, sets each of 
+ * these values in an object, and displays the values.
+ *
+ */
+
+public class Specs {
+    int height = 7 , weight = 8, depth = 9;
+    public static void main(String args[]) {
+    }
+}
+
+
