e50811877dbd21e995ea5411ef270ba3e4ad4c7c
touruni <touruni@gmail.com>
2012-01-05 15:55:21 -0800

    add

--- exercises/7/program/modded/NegativeNumberException.java
+++ exercises/7/program/modded/NegativeNumberException.java
@@ -4,4 +4,7 @@ public class NegativeNumberException extends Exception {
     public NegativeNumberException() {}
 
     public NegativeNumberException(String msg) {
+        super(msg);
+    }
+}
         
